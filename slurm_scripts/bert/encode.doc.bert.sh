106cb2a7b63997ea331ea617e6bf467b5a950232
DylanJu <jju8@jh.edu>
2025-10-10 11:59:39 -0400

    Update bert encoding

--- slurm_scripts/bert/encode.doc.bert.sh
+++ slurm_scripts/bert/encode.doc.bert.sh
@@ -5,24 +5,23 @@
 #SBATCH --mem=32
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=72:00:00
+#SBATCH --array=0-10%2
+#SBATCH --time=3-00:00:00
 #SBATCH --output=%x-%j.out
 
 module load anaconda3/2024.2
 conda activate crc
 
 model_dir=${HOME}/models/bert-msmarco-psg/checkpoint-45000
+shard=${SLURM_ARRAY_TASK_ID}
 
-for s in $(seq -f "%02g" 0 19)
-do
-CUDA_VISIBLE_DEVICES=0 python -m tevatron.retriever.driver.encode \
+python -m tevatron.retriever.driver.encode \
   --output_dir=temp \
   --model_name_or_path ${model_dir} \
   --fp16 \
   --per_device_eval_batch_size 156 \
   --passage_max_len 128 \
   --dataset_name Tevatron/msmarco-passage-corpus \
-  --encode_output_path /home/hltcoe/jhueiju/temp/output_bert/bert-msmarco-psg/corpus_msmarco-psg.pkl$each \
-  --dataset_number_of_shards 20 \
-  --dataset_shard_index ${s}
-done
+  --encode_output_path /home/hltcoe/jhueiju/temp/output_bert/bert-msmarco-psg/corpus.bert-msmarco-psg.pkl$shard \
+  --dataset_number_of_shards 10 \
+  --dataset_shard_index $shard
