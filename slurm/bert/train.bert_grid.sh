0d5a975263f1b7200726448e8ac121094498827f
DylanJu <jju8@jh.edu>
2025-10-21 17:26:49 -0400

    update dev set

--- slurm/bert/train.bert_grid.sh
+++ slurm/bert/train.bert_grid.sh
@@ -8,37 +8,37 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --output=%x-%j.out
 
+# Multi-GPU encoding
 module load anaconda3/2024.2
 conda activate crc
 
-model_dir=${HOME}/models/bert-msmarco-psg.b32_n256.100k
-
-mkdir -p ${model_dir}
+mkdir -p ${HOME}/models/bert-msmarco-psg
+model_dir=${HOME}/models/bert-msmarco-psg
 
 # Start experiments
-CUDA_VISIBLE_DEVICES=0 python -m tevatron.retriever.driver.train \
+python -m tevatron.retriever.driver.train \
     --output_dir ${model_dir} \
     --model_name_or_path bert-base-uncased \
     --save_steps 5000 \
     --dataset_name Tevatron/msmarco-passage-new \
     --corpus_name Tevatron/msmarco-passage-corpus-new \
     --per_device_train_batch_size 32 \
+    --train_group_size 8 \
+    --dataloader_num_workers 1 \
+    --learning_rate 1e-5 \
+    --query_max_len 32 \
+    --passage_max_len 256 \
+    --max_steps 50000 \
+    --logging_steps 10 \
+    --warmup_steps 5000 \
+    --attn_implementation sdpa \
+    --overwrite_output_dir \
     --prediction_loss_only True \
     --eval_strategy steps \
     --do_eval True \
     --eval_dataset_name DylanJHJ/Qrels \
     --eval_dataset_split msmarco_passage.trec_dl_2019 \
-    --eval_group_size 8 \
-    --per_device_eval_batch_size 64 \
+    --eval_group_size 100 \
+    --per_device_eval_batch_size 16 \
     --eval_steps 100 \
-    --train_group_size 8 \
-    --dataloader_num_workers 1 \
-    --learning_rate 1e-5 \
-    --query_max_len 32 \
-    --passage_max_len 196 \
-    --max_steps 100000 \
-    --logging_steps 10 \
-    --attn_implementation sdpa \
-    --overwrite_output_dir \
-    --warmup_steps 10000 \
-    --run_name nv.bert-base.msmarco-passage.b${bsz}_n${nsample}.1e-5.10k_100k
+    --run_name NV-bert-base.msmarco-passage.b32_n256.1e-5.5k_50k
