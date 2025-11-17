0e0651cada0e8ac465d22d67b0d6713e531f4448
DylanJoo <dylanjootw@gmail.com>
2025-11-17 19:52:15 +0100

    upload entire beir corpus

---
+++ src/evaluation-data-curation/create_beir_corpus.py
@@ -0,0 +1,50 @@
+import os
+import ir_datasets
+from datasets import DatasetDict, Dataset
+
+dataset_dict = {}
+home_dir = os.path.expanduser("~/")
+
+## QRELS from TREC
+split_names = [
+    "beir.arguana",
+    "beir.climate_fever",
+    "beir.dbpedia_entity",
+    "beir.fever",
+    "beir.fiqa",
+    "beir.hotpotqa",
+    "beir.nfcorpus",
+    "beir.nq",
+    "beir.quora",
+    "beir.scidocs",
+    "beir.scifact",
+    "beir.trec_covid",
+    "beir.webis_touche2020",
+]
+
+for split in split_names:
+
+    print("Creating BEIR corpus for:", split)
+    tag = split.replace("_", "-")
+    tag = tag.replace(".", "/")
+    d = ir_datasets.load(tag)
+
+    document = {}
+    for doc in d.docs_iter():
+        docid = doc.doc_id
+        doctext = doc.text
+        document[docid] = doctext
+
+    dataset_dict[split] = []
+    for docid in document:
+        dataset_dict[split].append({
+            'docid': docid,
+            'title': "",
+            'text': document[docid],
+            'source': 'beir',
+        })
+
+
+## Transform to dataset
+dataset = DatasetDict( {key: Dataset.from_list(dataset_dict[key]) for key in dataset_dict})
+dataset.push_to_hub("DylanJHJ/beir-corpus")
